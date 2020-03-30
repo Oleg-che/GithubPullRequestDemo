9035d15d69b280287701be356d33d5ed4c4ad877
Gerzonianzes <ol.ccherednik@gmail.com>
2020-03-30 15:14:29 +0300

    pull

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
 
+    private static int listSize = 10;
+
     public static void main(String[] args) {
-        // show me you Stream API power
-        System.out.println("test");
+        //Среднеарефметическое 
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < listSize ; i++) {
+            list.add(random.nextInt(100) - 1);
+        }
+        System.out.println(list);
+        var average = list.stream().mapToInt((s) -> Integer.parseInt(String.valueOf(s))).average();
+        System.out.println(average);
+
+
     }
 
 }
